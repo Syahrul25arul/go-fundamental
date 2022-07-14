a285f92d34a113409851d662485d20be426b8b48
syahrul25arul <syahrul25arul@gmail.com>
2022-07-14 23:13:19 +0700

    episode-9 belajar type declaration

---
+++ type-declaration.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	type noKTP string // noKTP adalah alias dari tipe data string yang
+	type Married bool
+
+	var noKtpHendrik noKTP = "2235jkhkjas09235"
+	var marriedStatus bool = true
+	fmt.Println(noKtpHendrik)
+	fmt.Println(marriedStatus)
+
+}
