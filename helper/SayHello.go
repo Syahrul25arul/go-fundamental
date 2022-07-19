bee391d6ae92bbe078ec44907682416a2ce504be
syahrul25arul <syahrul25arul@gmail.com>
2022-07-19 09:01:30 +0700

    episode-45 belajar access modifier

--- helper/SayHello.go
+++ helper/SayHello.go
@@ -2,6 +2,9 @@ package helper
 
 import "fmt"
 
+// var version = 1 // variabel yang di awali dengan huruf besar tidak bisa digunakan di pacakge lain
+var Application = "Belajar golang"
+
 /*
 * package helper menandakan bahwa source code ini sedang berada di direktori helper
 * dalam sebuah package atau helper, tidak boleh ada nama function yang sama
@@ -11,4 +14,8 @@ import "fmt"
 
 func SayHello(name string) {
 	fmt.Println("Hello", name)
+}
+
+func sayGoodBy(name string) { // function yang di awali dengan huruf kecil tidak bisa digunakna di huruf lain
+	fmt.Println("Gooby", name)
 }
--- import.go
+++ import.go
@@ -1,7 +1,15 @@
 package main
 
-import "belajar-golang-dasar/helper"
+import (
+	"belajar-golang-dasar/helper"
+	"fmt"
+)
 
 func main() {
 	helper.SayHello("Hendrik")
+	// helper.sayGoodBy("hendrik") // function yang di awali dengan huruf kecil, tidak bisa digunakan di package lain
+
+	fmt.Println(helper.Application)
+	// fmt.Println(helper.version) // variabel yang di awali dengan huruf besar tidak bisa digunakan di pacakge lain
+
 }
